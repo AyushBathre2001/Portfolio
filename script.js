b7654efb0f1196333478e0df15a7b5f1710d4298
Ayush Bathre <ayushbathre3101@gmail.com>
2023-03-18 09:41:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+gsap.from("#yoyo",{
+    y:"20px",
+    yoyo:true,
+    repeat:-1,
+    duration:1
+})
+
+let tl1 = gsap.timeline()
+
+
+tl1.from("#fontchange",{
+    opacity:0,
+    delay:-1,
+    onStart:function(){
+        $('#fontchange').textillate({
+            in:{
+                effect:'flash',
+                reverse:true
+              
+            },
+            
+          
+        })
+    }
+})
+
+.from("#txtl2",{
+    opcacity:0,
+    onStart:function(){
+        $('#txtl2').textillate({
+            in:{
+                effect:'flash',
+                sequence:true
+              
+            }
+          
+        })
+    },
+    delay:-1,
+    
+})
+.from("#txtl3",{
+    opacity:0,
+    onStart:function(){
+        $('#txtl3').textillate({
+            in:{
+                effect:'fadeInUp',
+                sequence:true,
+                delay:10
+              
+            }
+          
+        })
+    },
+    delay:1
+    
+})
+.from(".txtbox button",{
+    y:'20px',
+    opacity:0,
+    duration:1,
+    delay:1
+})
+.from('.navbar nav ul li',{
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+
+})
